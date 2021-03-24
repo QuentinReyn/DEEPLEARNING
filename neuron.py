b96b2e3c876f2af6cbc5cb9fefdf29d8ccd35e44
QuentinReynaud <quentin.reynaud99@gmail.com>
2021-03-24 11:19:30 +0100

    gradient and error loss implemented

--- neuron.py
+++ neuron.py
@@ -9,8 +9,8 @@ class Neuron():
     def __init__(self):
         np.random.seed(10)
         # r.seed(10)
-        self.w1 = r.random()
-        self.w2 = r.random()
+        self.w1 = 0.5
+        self.w2 = 0.5
 
     # prend entrées, qui peuvent etre une photo de chat
     def forward(self, x1, x2):
@@ -33,37 +33,25 @@ class Neuron():
             matResult.append(sum(x))
         return matResult
 
-    def think(self, inputs):
-        # passing the inputs via the neuron to get output
-        # converting values to floats
+    def meanSquareErrorLoss(self,expected,result):
+        return np.square(np.subtract(expected, result)).mean()
 
-        inputs = inputs.astype(float)
-        output = self.sigmoid(np.dot(inputs, self.synaptic_weights))
-        return output
-
-    def train(self, training_inputs, training_outputs, training_iterations):
-
-        # training the model to make accurate predictions while adjusting weights continually
-        for iteration in range(training_iterations):
-            # siphon the training data via  the neuron
-            output = self.think(training_inputs)
-
-            # computing error rate for back-propagation
-            error = training_outputs - output
-
-            # performing weight adjustments
-            adjustments = np.dot(training_inputs.T, error *
-                                 self.sigmoid_derivative(output))
-
-            self.synaptic_weights += adjustments
-
+    def gradient(self,x1,x2,expected,result):
+        gradients = []
+        nw1 = self.w1 - (x1 * ((2 * expected) - (2*result)))
+        nw2 = self.w2 - (x2 * ((2 * expected) - (2*result)))
+        gradients.append(nw1)
+        gradients.append(nw2)
+        return gradients
 
 if __name__ == "__main__":
     myneuron = Neuron()
-    # on entre 10 photos de chats
-    x1 = 4
-    x2 = 40
-    x = myneuron.forward(x1, x2)
+    x1 = 3
+    x2 = 4
+    x = myneuron.forward(x1, x2) #result
+    expected = x1+x2
+    print(myneuron.meanSquareErrorLoss(x1+x2,x))
+    print(myneuron.gradient(x1,x2,expected,x))     
     # on a soit l'un soit l'autre, soit entre les 2
     # donc il prédit l'un ou l'autre ou il ne sait pas lequel c'est
-    print('x1 = {} et x2 = {}, res = {}'.format(x1, x2, myneuron.activation(x)))
+    # print('x1 = {} et x2 = {}, res = {}'.format(x1, x2, myneuron.activation(x)))
